5848f4f22099ce8456c55d1f662b5bb4e556a63b
samsonan <samsonan.info@gmail.com>
2017-08-20 17:58:47 +0300

    using adapters instead of GW

--- src/main/java/com/samsonan/tcpserver/SpringBootTcpServerApplication.java
+++ src/main/java/com/samsonan/tcpserver/SpringBootTcpServerApplication.java
@@ -2,7 +2,12 @@ package com.samsonan.tcpserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.config.EnableIntegration;
 
+// example of XML configuration:
+//@ImportResource("META-INF/spring/integration/tcpClientServerDemo-context.xml")
+//@EnableIntegration
 @SpringBootApplication
 public class SpringBootTcpServerApplication {
 
